34934397fac7816d8722cfaddc76e1d7435625ad
goku <wkurts247@gmail.com>
2023-07-01 18:26:06 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector("form");
+  form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    newTodo(e.target.new-task-description.value)
+  })
 });
+
+function newTodo(todo) {
+  let p = document.createElement("p");
+  let btn = document.createElement("button")
+  p.textContent = `${todo}`;
+  btn.addEventListener("click",handleDelete)
+  p.appendChild(btn);
+  console.log(p);
+  document.querySelector("#tasks").appendChild(p)
+;
+}
+function handleDelete(e) {
+  e.target.parentNode.remove();
+}
+
